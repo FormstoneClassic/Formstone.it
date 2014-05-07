8051f6083729e960f81a1dd1a8abd4030db0f03c
Ben Plum <mr@benplum.com>
2014-05-07 17:38:02 -0400

    Adding new ads layout

--- themes/formstone/js/main.js
+++ themes/formstone/js/main.js
@@ -14,12 +14,14 @@
 			Site.transitionEvent = Site._getTransitionEvent();
 			Site.transitionSupported = (Site.transitionEvent !== false);
 
+			/*
 			$.pronto({
 				selector: "a:not(.no-pronto)",
 				tracking: {
 					manager: true
 				}
 			});
+			*/
 
 			$.rubberband({
 				minWidth: [ 320, 500, 740, 980, 1220 ],
